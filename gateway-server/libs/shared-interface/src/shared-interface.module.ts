370fb0e9d7a4a729418e91a3708621fda3986b4c
Neezar <rforlyffe@gmaicl.com>
2022-01-12 23:28:55 +0800

    init backend microservice

--- gateway-server/libs/shared-interface/src/shared-interface.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { SharedInterfaceService } from './shared-interface.service';
-
-@Module({
-  providers: [SharedInterfaceService],
-  exports: [SharedInterfaceService],
-})
-export class SharedInterfaceModule {}
