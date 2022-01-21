ff7eddaa03b66431ccc3dcd6860888edb1ad5195
Neezar <rforlyffe@gmaicl.com>
2022-01-21 23:15:57 +0800

    testing websocket

--- microfrontend/packages/root-app/src/app/pages/Home/Home.page.tsx
+++ microfrontend/packages/root-app/src/app/pages/Home/Home.page.tsx
@@ -1,10 +1,11 @@
 import Sidebar from "@component/SIdebar/Sidebar";
+import useSocket from "@store/websockets/websockets";
 import React from "react";
 import { Outlet, useLocation } from "react-router-dom";
 
 const Home: React.FC = () => {
   const location = useLocation();
-
+  useSocket();
   return (
     <div tw="min-h-screen flex flex-row bg-dark-main">
       <Sidebar />
---
+++ microfrontend/packages/root-app/src/app/store/useSocket.store.ts
@@ -0,0 +1,22 @@
+import { toast } from "react-toastify";
+import { Socket } from "socket.io-client";
+import create from "zustand";
+import { useHistory } from "../../bootstrap";
+import agent from "../api/agent";
+import { IAuth } from "./types/auth.types";
+import { combineAndImmer } from "./types/combine-Immer";
+
+export const useSocketStore = create(
+  combineAndImmer(
+    {
+      socket: null as Socket | null,
+    },
+    (set, get) => ({
+      setSocket: async (socketObj: Socket) => {
+        set((s) => {
+          s.socket = socketObj;
+        });
+      },
+    })
+  )
+);
