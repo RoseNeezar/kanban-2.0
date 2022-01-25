371ed1fa9b9aceaec10e6032c3f9d2f815208b02
Neezar <rforlyffe@gmaicl.com>
2022-01-25 23:26:08 +0800

    add basic dnd

--- microfrontend/packages/kanban/src/app/pages/Home/Home.page.tsx
+++ microfrontend/packages/kanban/src/app/pages/Home/Home.page.tsx
@@ -1,15 +1,7 @@
 import React from "react";
-import { useLocation } from "react-router-dom";
 
-const Home: React.FC = () => {
-  const location = useLocation();
-
-  return (
-    <div tw="min-h-screen bg-dark-main flex justify-center items-center w-full text-purple-300">
-      beats
-      <div tw="w-96 h-96  bg-green-200">boxes</div>
-    </div>
-  );
+const Home = () => {
+  return <div>Home</div>;
 };
 
 export default Home;
